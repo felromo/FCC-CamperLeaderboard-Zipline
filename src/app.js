46e0c321b9d3d23425a0ab0e03a0b41a84eac580
Tyrantz <raymanv3@gmail.com>
2016-03-28 10:33:50 +0000

    adds all the needed files for development

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var App = React.createClass({
+  render() {
+    return (
+      <div className="leader-board">
+        <h1 className="leader-board-title">LeaderBoard</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
